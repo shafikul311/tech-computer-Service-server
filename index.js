8a366e5362b7b6db971cd71fb7d86e03e8f5e001
“shafikulislam067@gmail.comgit config --global user.name “shafikul islam <shafikulislam067@gmail.comgit config --global user.email shafikulislam067@gmail.com>
2021-04-19 13:49:20 +0600

    added booking route

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const ObjectID = require("mongodb").ObjectID;
 const port = process.env.PORT || 5080
 
 
@@ -21,11 +22,22 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 client.connect(err => {
   const serviceCollection = client.db('service-provider').collection("service-collection");
   const reviewCollection = client.db('service-provider').collection("review-collection");
+  const bookingCollection = client.db('service-provider').collection("booking-collection");
   // console.log('working')
 
 
+  // Get Service from Database using id
+  app.get("/addService/:id", (req, res) => {
+    const id = ObjectID(req.params.id);
+    // console.log(id)
+    serviceCollection .find({ _id: id }).toArray((err, service) => {
+      res.send(service);
+    });
+  });
 
 
+
+// add Data review collection
   app.get("/addReview", (req, res) => {
     reviewCollection.find().toArray((err, items) => {
       res.send(items);
@@ -43,7 +55,38 @@ client.connect(err => {
 
 
 
+ // get data for all booking
+  app.get("/addBooking", (req, res) => {
+    bookingCollection.find().toArray((err, document) => {
+      res.send(document);
+    });
+  });
 
+
+
+        // get data from booking collection
+    app.get("/addBooking", (req, res) => {
+      // console.log(req.query.email)
+      bookingCollection.find({ email: req.query.email })
+        .toArray((err, document) => {
+          res.send(document);
+        });
+    });
+
+
+  // booking collection
+  app.post("/addBooking", (req, res) => {
+    const newBooking = req.body;
+
+    bookingCollection.insertOne(newBooking).then((result) => {
+      console.log('inserted one booking',result.insertedCount)
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+
+
+// add Data Service collection
   app.get("/addService", (req, res) => {
     serviceCollection.find().toArray((err, items) => {
       res.send(items);
